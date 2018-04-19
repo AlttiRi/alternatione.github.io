0b651f9bb8ad3e45c68a0224f7a317e7480ef184
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2018-04-19 15:26:27 +0300

    Update header, fonts

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@ function simple() {
 }
 
 simple();
+
+
+let isHover = false;
+
+let elem = document.querySelector("div.nav-btn");
+let links = document.querySelector(".nav-links-wrapper");
+console.log(elem);
+
+
+elem.addEventListener("touchstart", function(e) {
+
+  if (isHover) {
+    links.setAttribute("style", "display:none;");
+    isHover = false;
+  } else {
+    links.setAttribute("style", "");
+    isHover = true;
+  }
+
+});
+
+elem.addEventListener("mouseleave", function(e) {
+
+  isHover = false;
+
+});
