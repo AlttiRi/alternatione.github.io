2ea91a50f75e0fc6ca3ac59fa67cfe27e9ed7f50
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2018-04-20 14:08:24 +0300

    Fix by JS transient animation bug
    Ибо при ширине окна < 680px начальное значение visibility: visible у
    .nav-wrapper и происходила "анимация" на hidden, а она нужна лишь при
    закрытии меню, при "un-hover". А не при ресайзе окна.
    
    Т.е. при ресайзе в < 680px меню сразу же становится visibility:hidden.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,69 @@
-'use strict';
-
-
-testConsoleOutput();
+"use strict";
 
 
 
+function addMenuTransient() {
+
+    let widthWhenMenu = 680; // Ширина, при которой появляется мобильное меню
+
+    window.addEventListener("DOMContentLoaded", function () {
+        let intFrameWidth = window.innerWidth;
+
+        if (intFrameWidth <= widthWhenMenu) {
+            let navWrapper = document.querySelector(".nav-wrapper");
+            if (!navWrapper) {
+                return;
+            }
+            navWrapper.classList.add("ready-for-transient");
+        }
+    });
+
+    window.addEventListener("resize", function () {
+
+        let navWrapper = document.querySelector(".nav-wrapper");
+        if (!navWrapper) {
+            return;
+        }
+        let intFrameWidth = window.innerWidth;
+
+        // let elem = document.getElementById("logger");
+        // if (!elem) {
+        //     return;
+        // }
+        // elem.innerText = `Width ${intFrameWidth.toString()}`;
+
+        if (intFrameWidth <= widthWhenMenu) {
+            navWrapper.classList.add("ready-for-transient");
+        } else {
+            navWrapper.classList.remove("ready-for-transient");
+        }
+    });
+
+}
+
+
+
+function swap() {
+    let elem = document.getElementById("swappable-content");
+    elem.classList.toggle("hidden");
+}
 
 /**
  * По клику  по #swapper
  * уберает/показывает содержимое #swappable-content
  */
-(function addSwapperListener() {
+function addSwapperListener() {
 
-  let elem = document.getElementById("swapper");
+    let elem = document.getElementById("swapper");
 
-  if (!elem) {
-    return;
-  }
+    if (!elem) {
+        return;
+    }
 
-  elem.addEventListener("click", swap);
+    elem.addEventListener("click", swap);
 
-})();
-
-function swap() {
-  let elem = document.getElementById("swappable-content");
-  elem.classList.toggle("hidden");
 }
+
 
 
 /**
@@ -36,43 +74,50 @@ function swap() {
  * чтобы стили:hover при его наличии не работали. (:not(no-hover))
  *
  */
-(function makeBetterMobileMenu() {
+function addClosingMenuOnTouch() {
 
-  let isHover = false;
+    let isHover = false;
 
-  let btn = document.querySelector(".nav-btn");
-  let wrapper = document.querySelector(".nav-wrapper");
+    let btn = document.querySelector(".nav-btn");
+    let wrapper = document.querySelector(".nav-wrapper");
 
-  if (!btn || !wrapper) {
-    return;
-  }
-
-
-  btn.addEventListener("touchstart", function (ev) {
-
-    if (isHover) {
-      wrapper.classList.add("no-hover");
-      isHover = false;
-    } else {
-      wrapper.classList.remove("no-hover");
-      isHover = true;
+    if (!btn || !wrapper) {
+        return;
     }
 
-  }, {passive: true});
 
-  wrapper.addEventListener("mouseleave", function (ev) {
+    btn.addEventListener("touchstart", function () {
 
-    isHover = false;
+        if (isHover) {
+            wrapper.classList.add("no-hover");
+            isHover = false;
+        } else {
+            wrapper.classList.remove("no-hover");
+            isHover = true;
+        }
 
-  });
+    }, {passive: true});
 
-})();
+    wrapper.addEventListener("mouseleave", function () {
+
+        isHover = false;
+
+    });
+
+}
 
 
 /**
- * Просто для проверки работs JS.
+ * Просто для проверки работы JS.
  */
-
 function testConsoleOutput() {
-  console.log("It's working [AlttiRi.github.io]");
+    console.log("It's working [AlttiRi.github.io]");
 }
+
+
+testConsoleOutput();
+
+addSwapperListener();
+
+addClosingMenuOnTouch();
+addMenuTransient();
