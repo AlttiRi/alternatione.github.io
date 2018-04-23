c5245cc61681318b270427128e45b894d9f29cf3
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2018-04-23 17:52:58 +0300

    Add sitemap.xml and package.json

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const siteURL = "https://alttiri.github.io";
+
+
+
+console.log("-- sitemap-generator --");
+
+const SitemapGenerator = require("sitemap-generator");
+
+// create generator
+const generator = SitemapGenerator(siteURL, {
+    stripQuerystring: false
+});
+
+// register event listeners
+generator.on("done", () => {
+    console.log("sitemap.xml created");
+});
+
+// start the crawler
+generator.start();
